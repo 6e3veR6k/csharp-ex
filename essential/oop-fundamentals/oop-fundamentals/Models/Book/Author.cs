e80dec1080e4de0d56fba8f3780e092188f2bd75
Matvey Sokolov <bezyn4uk@hotmail.com>
2021-02-03 11:29:14 +0200

    Create class Book

---
+++ essential/oop-fundamentals/oop-fundamentals/Models/Book/Author.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_fundamentals.Models.Book
+{
+    public class Author
+    {
+        private string fullName;
+
+        public string FullName
+        {
+            get => fullName ?? "No Name";
+            set
+            {
+                if (String.IsNullOrEmpty(value)) throw new NullReferenceException();
+                    
+                fullName = value;
+            }
+        }
+
+        public virtual void Show()
+        {
+            Console.ResetColor();
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine($"Author name: {FullName}");
+        }
+    }
+}
---
+++ essential/oop-fundamentals/oop-fundamentals/Models/Book/Content.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_fundamentals.Models.Book
+{
+    public class Content
+    {
+        private string innerText;
+
+        public string InnerText
+        {
+            get => innerText ?? "...";
+            set
+            {
+                if (String.IsNullOrEmpty(value)) throw new NullReferenceException();
+
+                innerText = value;
+            }
+        }
+
+        public virtual void Show()
+        {
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"{InnerText}");
+        }
+    }
+}
