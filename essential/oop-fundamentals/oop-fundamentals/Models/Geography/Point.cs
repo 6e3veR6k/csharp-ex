63a43c6558457a99f263a9fa01f0ae60bb55bb28
Matvey Sokolov <bezyn4uk@hotmail.com>
2021-02-10 11:33:51 +0200

    create class Point

---
+++ essential/oop-fundamentals/oop-fundamentals/Models/Geography/Point.cs
@@ -0,0 +1,20 @@
+﻿namespace oop_fundamentals.Models.Geography
+{
+    public class Point
+    {
+        private int x;
+        private int y;
+        private string name;
+
+        public int X { get => x; }
+        public int Y { get => y; }
+        public string Name { get => name; }
+
+        public Point(int x, int y, string name)
+        {
+            this.x = x;
+            this.y = y;
+            this.name = name;
+        }
+    }
+}
