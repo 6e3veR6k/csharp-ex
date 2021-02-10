23ed4a87c6a4804d0dd1e513e6203eece575c5d7
Matvey Sokolov <bezyn4uk@hotmail.com>
2021-02-10 17:28:02 +0200

    create class Figure

--- essential/oop-fundamentals/oop-fundamentals/Models/Geography/Point.cs
+++ essential/oop-fundamentals/oop-fundamentals/Models/Geography/Point.cs
@@ -1,6 +1,8 @@
-﻿namespace oop_fundamentals.Models.Geography
+﻿using System;
+
+namespace oop_fundamentals.Models.Geography
 {
-    public class Point
+    public class Point: IComparable
     {
         private int x;
         private int y;
@@ -15,6 +17,25 @@
             this.x = x;
             this.y = y;
             this.name = name;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Point firstPoint = obj as Point;
+
+            if(firstPoint != null)
+            {
+                double firstPointDistanseFromZero = Math.Sqrt(Math.Pow(firstPoint.X, 2) + Math.Pow(firstPoint.Y, 2));
+                double thisPointDistanseFromZero = Math.Sqrt(Math.Pow(this.X, 2) + Math.Pow(this.Y, 2));
+
+                return thisPointDistanseFromZero.CompareTo(firstPointDistanseFromZero);
+            }
+            else
+            {
+                throw new ArgumentException("Object is not a Point.");
+            }
         }
     }
 }
