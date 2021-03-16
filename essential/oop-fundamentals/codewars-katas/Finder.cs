1608f3b3790f7d1c1f79abd395750f4e3e5d7658
Matvey Sokolov <bezyn4uk@hotmail.com>
2021-03-16 17:37:06 +0200

    (Closed #18) Create Class Finder

---
+++ essential/oop-fundamentals/codewars-katas/Finder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace codewars_katas
+{
+    public class Finder
+    {
+        public static int FindDeletedNumber(List<int> startingList, List<int> mixedList)
+        {
+            // Your code here
+            int[] result = startingList.Where(x => !mixedList.Any(t => t == x)).ToArray();
+            int a = result.Length == 0 ? 0 : result[0];
+            return a;
+        }
+
+    }
+}
---
+++ essential/oop-fundamentals/test.codewars-katas/Finder_Tests.cs
@@ -0,0 +1,35 @@
+﻿using codewars_katas;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace test.codewars_katas
+{
+    [TestFixture]
+    public class Finder_Tests
+    {
+        [Test]
+        public void MissingNumberTest()
+        {
+            Assert.AreEqual(6, Finder.FindDeletedNumber(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new List<int>() { 5, 7, 1, 9, 4, 8, 2, 3 }));
+            Assert.AreEqual(1, Finder.FindDeletedNumber(new List<int>() { 1, 2 }, new List<int>() { 2 }));
+            Assert.AreEqual(6, Finder.FindDeletedNumber(new List<int>() { 1, 2, 3, 4, 5, 6 }, new List<int>() { 5, 4, 2, 3, 1 }));
+            Assert.AreEqual(10, Finder.FindDeletedNumber(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new List<int>() { 9, 4, 3, 6, 8, 2, 1, 5, 7 }));
+            Assert.AreEqual(1, Finder.FindDeletedNumber(new List<int>() { 1 }, new List<int>()));
+        }
+
+        [Test]
+        public void NoMissingNumberTest()
+        {
+            Assert.AreEqual(0, Finder.FindDeletedNumber(new List<int>() { 1, 2, 3, 4, 5, 6, 7 }, new List<int>() { 2, 3, 6, 1, 5, 4, 7 }));
+            Assert.AreEqual(0, Finder.FindDeletedNumber(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new List<int>() { 5, 7, 6, 9, 4, 8, 1, 2, 3 }));
+        }
+
+        [Test]
+        public void NilTest()
+        {
+            Assert.AreEqual(0, Finder.FindDeletedNumber(new List<int>(), new List<int>()));
+            Assert.AreEqual(0, Finder.FindDeletedNumber(new List<int>(), new List<int>() { 10 }));
+        }
+    }
+}
