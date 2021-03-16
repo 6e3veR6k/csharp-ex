e41cf475699435cfb672e497ea21ae1a4faaba47
Matvey Sokolov <bezyn4uk@hotmail.com>
2021-03-16 15:48:24 +0200

    (Closed #15) Create test project

---
+++ essential/oop-fundamentals/codewars-katas/FireFighter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codewars_katas
+{
+    public class FireFighter
+    {
+        public static string FireFight(string s)
+        {
+            return s;
+        }
+    }
+}
